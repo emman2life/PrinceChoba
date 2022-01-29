94fa35f26083f18a93d841adf90f0a2688a06c9d
Imhontu Emmanuel <imhontu.emmanuel@yahoo.com>
2022-01-29 13:02:11 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+
+    // const modal = document.querySelector("")
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li')
+    burger.addEventListener('click', ()=>{
+
+      nav.classList.toggle('nav-active');  
+      //animate links
+      navLinks.forEach((link, index)=>{
+          if(link.style.animation){
+            link.style.animation='';
+          }
+          else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s` 
+
+          }
+        
+    });
+    //animate menu
+    burger.classList.toggle('cross');
+    });
+   
+}
+navSlide();
+// const app = ()=>{
+//     navSlide();
+// }
